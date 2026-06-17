27b3c7254209de4a35a60aaa608b9e7a593a690c
Mellow <pastelmarsh96@gmail.com>
2026-06-16 21:14:09 -0600

    Add: CreateMatchRequestDto.cs to let client and server communicate about match creation.

---
+++ src/hangman/HangmanGame.Core/Core/DTOs/CreateMatchRequestDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame.Core.Core.DTOs
+{
+    [DataContract]
+    public class CreateMatchRequestDto
+    {
+        [DataMember]
+        public int HostId { get; set; }
+
+        [DataMember]
+        public int WordId { get; set; }
+
+        [DataMember]
+        public bool IsLocalNetwork { get; set; }
+    }
+
+    [DataContract]
+    public class CreateMatchResponseDto
+    {
+        [DataMember]
+        public bool Success { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public int MatchId { get; set; }
+    }
+}
