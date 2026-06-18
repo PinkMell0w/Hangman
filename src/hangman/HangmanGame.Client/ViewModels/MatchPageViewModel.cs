c405f79847eac79fa1bff619948ff90c271e4b4a
Mellow <pastelmarsh96@gmail.com>
2026-06-18 01:39:05 -0600

    Modify: Laid groundwork for MatchPageViewModel, unsure if I'll use it as-is. Modify: MatchPage.xaml now uses commands from its viewmodel. Add: LeaveMatchRequestDto and LeaveMatchResponseDto to handle guesser leaving match.

--- src/hangman/HangmanGame.Client/ViewModels/MatchPageViewModel.cs
+++ src/hangman/HangmanGame.Client/ViewModels/MatchPageViewModel.cs
@@ -4,25 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace HangmanGame.Client.ViewModels
 {
     internal class MatchPageViewModel : BaseViewModel
     {
-        public bool IsHost { get; set; }
-        public Visibility HostControlsVisibility => IsHost ? Visibility.Visible : Visibility.Collapsed;
+        private bool _isHost { get; set; }
+        private string _opponentName { get; set; }
+        private int _matchId { get; set; }
+        public Visibility HostControlsVisibility => _isHost ? Visibility.Visible : Visibility.Collapsed;
+
+        public bool IsNotHost => !_isHost;
+
+        public string OpponentName
+        {
+            get => _opponentName;
+            set { _opponentName = value; OnPropertyChanged(); }
+        }
+
+        public int MatchId
+        {
+            get => _matchId;
+            set { _matchId = value; OnPropertyChanged(); }
+        }
+
+        public ICommand ConfigureMatchCommand { get; }
+        public ICommand LeaveMatchCommand { get; }
+        public ICommand StartMatchCommand { get; }
+        public ICommand SendMessageCommand { get; }
 
         public MatchPageViewModel()
         {
-            IsHost = true;
+            _isHost = true;
         }
 
         public MatchPageViewModel(int matchId)
         {
+
             LoadMatch(matchId);
         }
 
+        private void ConfigureMatch()
+        {
+            // TODO
+        }
+
+        private void LeaveMatch()
+        {
+            // TODO
+        }
+
+        private void StartMatch()
+        {
+            // TODO
+        }
+
+        private void SendMessage()
+        {
+            // TODO
+        }
+
         private void LoadMatch(int matchId)
+        {
+            // TODO
+        }
+
+        private void HandleOpponentName()
         {
             // TODO
         }
---
+++ src/hangman/HangmanGame.Core/Core/DTOs/LeaveMatchRequestDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame.Core.Core.DTOs
+{
+    [DataContract]
+    public class LeaveMatchRequestDto
+    {
+        [DataMember]
+        public int MatchId { get; set; }
+
+        [DataMember]
+        public int UserId { get; set; }
+    }
+
+    [DataContract]
+    public class LeaveMatchResponseDto
+    {
+        [DataMember]
+        public bool Success { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
