95e87468d8a74cf59c577634a6989e8887e7edf1
Mellow <pastelmarsh96@gmail.com>
2026-06-17 23:04:28 -0600

    Add: JoinMatchRequestDto and JoinMatchResponseDto, as both are needed to join a match and weren't existing prior.

---
+++ src/hangman/HangmanGame.Core/Core/DTOs/JoinMatchRequestDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame.Core.Core.DTOs
+{
+    [DataContract]
+    public class JoinMatchRequestDto
+    {
+        [DataMember]
+        public int MatchId { get; set; }
+
+        [DataMember]
+        public int UserId { get; set; }
+    }
+
+    [DataContract]
+    public class JoinMatchResponseDto
+    {
+        [DataMember]
+        public bool Success { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
